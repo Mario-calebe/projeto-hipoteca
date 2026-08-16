d86ecdb5f3342f3f91463bcd19f59982f2789866
MarioSilva <calebemp33@gmail.com>
2026-08-16 17:11:43 -0300

    Atualização
    Integração na validação de dados e em melhor acessibilidade ao toco, dos radios button

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 let amount = document.getElementById('mort-amount')
 let term = document.getElementById('mort-term')
 let rate = document.getElementById('rate')
-let cal = document.querySelectorAll('div.cal')
-let r1 = document.getElementById('repay')
-let r2 = document.getElementById('interest-only')
-
+let p = document.querySelectorAll('p.required')
+let div = document.querySelectorAll(`div.required`)
+let label = document.querySelectorAll(`label.required`)
+let radios = document.querySelectorAll('input[type="radio"]')
+let inputs = [amount,term,rate]
 function Calcular(){
-    if (Number(amount.value.length) == 0) {
-        cal[0].innerHTML += '<p class="required">This field is required</p>'
+    if (Number(amount.value == 0 )) {
+        p[0].style.display = 'block'
+        div[0].style.borderColor = `var(--Red)`
+        label[0].style.background = 'var(--Red)'
+        label[0].style.color = 'var(--White)'
+        amount.addEventListener('input', function() {
+            if (amount.value > 0) {
+                p[0].style.display = 'none'
+                div[0].style.borderColor = ``
+                label[0].style.background = ''
+                label[0].style.color = ''
+            }
+        })
     }
-    if (Number(term.value.length) == 0) {
-        cal[1].innerHTML += '<p class="required">This field is required</p>'
+    if (Number(term.value == 0 )) {
+        p[1].style.display = 'block'
+        div[1].style.borderColor = `var(--Red)`
+        label[1].style.background = 'var(--Red)'
+        label[1].style.color = 'var(--White)'
+        term.addEventListener('input', function() {
+            if (term.value > 0) {
+                p[1].style.display = 'none'
+                div[1].style.borderColor = ``
+                label[1].style.background = ''
+                label[1].style.color = ''
+            }
+        })
     }
-    if (Number(rate.value.length) == 0) {
-        cal[2].innerHTML += '<p class="required">This field is required</p>'
+    if (Number(rate.value == 0 )) {
+        p[2].style.display = 'block'
+        div[2].style.borderColor = `var(--Red)`
+        label[2].style.background = 'var(--Red)'
+        label[2].style.color = 'var(--White)'
+        rate.addEventListener('input', function() {
+            if (rate.value > 0) {
+                p[2].style.display = 'none'
+                div[2].style.borderColor = ``
+                label[2].style.background = ''
+                label[2].style.color = ''
+            }
+        })
     }
-    if (r1) {
-        
+    if (!radios[0].checked && !radios[1].checked) {
+        p[3].style.display = 'block'
     }
 }
+radios.forEach(function(radioinvalido) {
+    radioinvalido.addEventListener('change', function (){
+        p[3].style.display = 'none'
+    })
+})
