05994b834a1dca66a641e04ca66144931acdbdb6
MarioSilva <calebemp33@gmail.com>
2026-08-20 16:59:04 -0300

    Projeto FINALIZADO

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let amount = document.getElementById('mort-amount')
 let term = document.getElementById('mort-term')
 let rate = document.getElementById('rate')
 
-let p = document.querySelectorAll('p.required')
-let div = document.querySelectorAll(`div.required`)
-let label = document.querySelectorAll(`label.required`)
+let mensagensErro = document.querySelectorAll('p.required')
+let campos = document.querySelectorAll(`div.required`)
+let labels = document.querySelectorAll(`label.required`)// Icones dos campos
 
 let radios = document.querySelectorAll('input[type="radio"]')
 let inputs = [amount,term,rate]
@@ -14,38 +14,54 @@ let resultado = document.querySelector('div.result')
 let h1card = document.querySelector('div.card > h1')
 let h2card = document.querySelector('div.card > h2')
 
+document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault()
+    //Chamar a função, eu criei a função, mas ela não pertence a um evento, agr ela é CHAMADA quando o evento de submit ocorre!
+    Calcular()
+})
+
 function Calcular(){
     let mortvalue = Number(amount.value)
     let anos = Number(term.value)
     let taxa = Number(rate.value)
     
     let meses = anos * 12
-    let Imeses = (taxa / 100) / 12 // isto é a taxa a.m
+    let taxamensal = (taxa / 100) / 12 // isto é a taxa a.m
     let monthlypayment
     let Payment
     let interest
+
+    let erro = false
     inputs.forEach(function(input, i) {
         if (Number(input.value) <= 0) {
-            p[i].style.display = 'block'
-            div[i].classList.add('div-required')
-            label[i].classList.add('label-required')
+            mensagensErro[i].style.display = 'block'
+            campos[i].classList.add('div-required')
+            labels[i].classList.add('label-required')
+            
+            erro = true
         }        
     })
 
     if (!radios[0].checked && !radios[1].checked) { 
-        p[3].style.display = 'block'
+        mensagensErro[3].style.display = 'block'
+        return
     }
 
+    if (erro) {
+        return
+    }
+
+
     if (radios[0].checked) {
-        monthlypayment = mortvalue * (Imeses * Math.pow(1 + Imeses, meses)) / ((Math.pow(1+Imeses,meses)) -1)    
+        monthlypayment = mortvalue * (taxamensal * Math.pow(1 + taxamensal, meses)) / ((Math.pow(1+taxamensal,meses)) -1)    
 
         Payment = monthlypayment * meses
         interest = Payment 
-        let total = interest.toLocaleString('en-Gb', {
+        let total = interest.toLocaleString('en-GB', {
             style: 'currency',
             currency: 'GBP'
         })
-        let mensal = monthlypayment.toLocaleString('en-Gb', {
+        let mensal = monthlypayment.toLocaleString('en-GB', {
             style: 'currency',
             currency: 'GBP'
         })
@@ -54,14 +70,14 @@ function Calcular(){
         h2card.innerText = `${total}`
 
     }else if(radios[1].checked){
-        monthlypayment = mortvalue * Imeses
+        monthlypayment = mortvalue * taxamensal
         interest = monthlypayment * meses + mortvalue
 
-        let total = interest.toLocaleString('en-Gb', {
+        let total = interest.toLocaleString('en-GB', {
             style: 'currency',
             currency: 'GBP'
         })
-        let mensal = monthlypayment.toLocaleString('en-Gb', {
+        let mensal = monthlypayment.toLocaleString('en-GB', {
             style: 'currency',
             currency: 'GBP'
         })
@@ -69,45 +85,43 @@ function Calcular(){
         h1card.innerText = `${mensal}`
         h2card.innerText = `${total}`
 
-        console.log('Mensal:', monthlypayment)
-        console.log('Juros:', interest)
-        console.log('Principal:', mortvalue)
+        // console.log('Mensal:', monthlypayment)
+        // console.log('Juros:', interest)
+        // console.log('Principal:', mortvalue)
     }
-        if ((amount.value > 0 && term.value > 0 && rate.value > 0) && radios[0].checked || radios[1].checked) {
-            pendente.style.display = 'none'
-            resultado.style.display = 'block'
-            
-    }
+    pendente.style.display = 'none'
+    resultado.style.display = 'block'
 
 }
 // Função para tirar erro quando escrever
 inputs.forEach( (input, i) => {
     input.addEventListener('input', function () {
         if (input.value > 0) {
-            p[i].style.display = 'none'
-            div[i].classList.remove('div-required')
-            label[i].classList.remove('label-required')            
+            mensagensErro[i].style.display = 'none'
+            campos[i].classList.remove('div-required')
+            labels[i].classList.remove('label-required')            
         }
     })
 })
 // Função de sensor para RADIO
 radios.forEach(function(radioinvalido) {
     radioinvalido.addEventListener('change', function() {
-        p[3].style.display = 'none'
+        mensagensErro[3].style.display = 'none'
     })
 })
 function Clear() {
     inputs.forEach(function(input,i) {
         input.value = ''
-        p[i].style.display = 'none'
-        div[i].classList.remove('div-required')
-        label[i].classList.remove('label-required')
+        mensagensErro[i].style.display = 'none'
+        campos[i].classList.remove('div-required')
+        labels[i].classList.remove('label-required')
     })
     radios.forEach(function(radio) {
         radio.checked = false
-        p[3].style.display = 'none'
+        mensagensErro[3].style.display = 'none'
     })
     resultado.style.display = 'none'
     pendente.style.display = 'flex'
     
 }
+
